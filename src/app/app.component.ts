691bdae82cd3a5827dce9f41dce73da236a0f9fc
allnightlong <allnightlong@allnightlong.ru>
2021-02-09 01:10:39 +0300

    basic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nonogram-angular-solver';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get('assets/data/source.json').subscribe(data => {
+      console.log(data);
+      // this.products = data;
+    });
+  }
 }
